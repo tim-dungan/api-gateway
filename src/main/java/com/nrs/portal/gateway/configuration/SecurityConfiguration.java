53e7def83f71d5ab7732fecf157b4d1b47d32c98
rutul <rutul.patel@nrsonline.com>
2023-10-03 08:01:18 -0400

    clean up

--- src/main/java/com/nrs/portal/gateway/configuration/SecurityConfiguration.java
+++ src/main/java/com/nrs/portal/gateway/configuration/SecurityConfiguration.java
@@ -2,14 +2,10 @@ package com.nrs.portal.gateway.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
-/**
- * @author Akash Patel (apatel)
- */
+
 @Configuration
 public class SecurityConfiguration {
 
